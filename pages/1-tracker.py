395f6bb65cfda6f725c301e53e3840c8c8335c62
MarcPoehler <marcpoehler@aol.com>
2024-12-24 15:33:51 +0100

    feat: added pages & no_score

--- pages/1-tracker.py
+++ pages/1-tracker.py
@@ -1,27 +1,5 @@
 import streamlit as st
 
-st.title("DDL Gametracker")
-st.write("Willkommen zum DDL Gametracker - wer spielt?")
-
-# Spieler-Auswahl
-player_one = st.selectbox("Spieler 1", ("Sven", "Takeshi", "Markus", "Marc"))
-player_two = st.selectbox("Spieler 2", ("Sven", "Takeshi", "Markus", "Marc"))
-game_mode = st.radio("301 oder 501?", [301, 501])
-
-if "player_one" not in st.session_state:
-    st.session_state["player_one"] = player_one
-if "player_two" not in st.session_state:
-    st.session_state["player_two"] = player_two
-if "players_info" not in st.session_state:
-    st.session_state["players_infos"] = [
-        {'player_name': player_one, 'points_left': game_mode, 'last_throw': []}, 
-        {'player_name': player_two, 'points_left': game_mode, 'last_throw': []}
-    ]
-
-
-if player_one and player_two and game_mode:
-    st.write(f"Es spielt {player_one} gegen {player_two}. Gamemode: {game_mode}. Viel Erfolg!")
-
 # Session State für Multiplikator, Treffer und Wurf-Speicher initialisieren
 if "multiplier" not in st.session_state:
     st.session_state["multiplier"] = 1
@@ -38,6 +16,8 @@ with st.container():
     single_bull = col1.button("Single bull", use_container_width=True)
     is_double = col2.button("Double", use_container_width=True)
     bullseye = col2.button("Bullseye", use_container_width=True)
+    no_score = st.button("No score", use_container_width=True)
+
 
     if is_triple:
         st.session_state["multiplier"] = 3
@@ -47,6 +27,8 @@ with st.container():
         st.session_state["number_hits"][st.session_state["current_throw"]] = 25
     elif bullseye:
         st.session_state["number_hits"][st.session_state["current_throw"]] = 50
+    elif no_score:
+        st.session_state["number_hits"][st.session_state["current_throw"]] = 0
 
     # Nummern-Buttons
     left, middle_left, middle_right, right = st.columns(4)
